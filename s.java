d660bafe8caae5c865774b44be174f92a47a3f95
Tanya Narayan <tanyanarayan5@gmail.com>
2026-06-01 23:28:34 +0530

    Add files via upload

---
+++ s.java
@@ -0,0 +1,23 @@
+public class s {
+    public static void main(String[] args) {
+        int[] arr = {2, 1, 5, 1, 3, 2};
+        int k = 3;
+
+        int windowSum = 0;
+
+        // First window
+        for (int i = 0; i < k; i++) {
+            windowSum += arr[i];
+        }
+
+        int maxSum = windowSum;
+
+        // Slide the window
+        for (int i = k; i < arr.length; i++) {
+            windowSum = windowSum - arr[i - k] + arr[i];
+            maxSum = Math.max(maxSum, windowSum);
+        }
+
+        System.out.println("Maximum Sum = " + maxSum);
+    }
+}
